25fd3dc47b38791d012a14b0bcebd75c3abd889b
mithunkj-eduate <mithunkj@myeduate.com>
2026-01-27 23:40:58 +0530

    changes

--- src/screens/TestPage.jsx
+++ src/screens/TestPage.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import axios from "axios";
+import React from "react";
+import { BASEURL, config } from "../config/config";
 
 const TestPage = () => {
+  const bulkImport = async (id) => {
+    try {
+      const res = await axios.post(`${BASEURL}/auth/bulkRegister`, {}, config);
+      const resData = res.data.data;
+      console.log(resData)
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div>
       TestPage
+      {/* <button onClick={bulkImport}>Click</button> */}
     </div>
-  )
-}
+  );
+};
 
-export default TestPage
+export default TestPage;
